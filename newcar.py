a2558a99a41af31e4bcf4341fd5e68687e3d045d
Rafey Rana <24100103@lums.edu.pk>
2024-08-26 00:39:05 +0500

    car class setup

---
+++ newcar.py
@@ -0,0 +1,56 @@
+
+import math
+import random
+import sys
+import os
+
+import neat
+import pygame
+
+
+
+
+
+
+# Constants
+# WIDTH = 1600
+# HEIGHT = 880
+
+WIDTH = 1920
+HEIGHT = 1080
+
+CAR_SIZE_X = 60    
+CAR_SIZE_Y = 60
+
+BORDER_COLOR = (255, 255, 255, 255) # Color To Crash on Hit
+
+current_generation = 0 # Generation counter
+
+class Car:
+
+    def __init__(self):
+        # Load Car Sprite and Rotate
+        self.sprite = pygame.image.load('car.png').convert() # Convert Speeds Up A Lot
+        self.sprite = pygame.transform.scale(self.sprite, (CAR_SIZE_X, CAR_SIZE_Y))
+        self.rotated_sprite = self.sprite 
+
+        # self.position = [690, 740] # Starting Position
+        self.position = [830, 920] # Starting Position
+        self.angle = 0
+        self.speed = 0
+
+        self.speed_set = False # Flag For Default Speed Later on
+
+        self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2] # Calculate Center
+
+        self.radars = [] # List For Sensors / Radars
+        self.drawing_radars = [] # Radars To Be Drawn
+
+        self.alive = True # Boolean To Check If Car is Crashed
+
+        self.distance = 0 # Distance Driven
+        self.time = 0 # Time Passed
+
+    def draw(self, screen):
+        screen.blit(self.rotated_sprite, self.position) # Draw Sprite
+        self.draw_radar(screen) #OPTIONAL FOR SENSORS
